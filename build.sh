85f86bea0c3a142a830dfff0a640bb4a9b1eeefd
Chris West (Faux) <git@goeswhere.com>
2012-01-04 21:08:06 +0000

    32-bit build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo yum -y install git gcc-c++.x86_64 libpcap-devel.x86_64 openssl-devel.x86_64 make
+sudo yum -y install git gcc-c++ libpcap-devel openssl-devel make
 (cd src && make -j && mv yapscan ..)
 
